75e016f43f07326849127f87c7452d5f3193744c
Samuel Manco <samuel.manco@udea.edu.co>
2026-09-17 16:10:35 -0500

    Permite CORS universal en WebConfig

--- src/main/java/com/natillera/config/WebConfig.java
+++ src/main/java/com/natillera/config/WebConfig.java
@@ -6,10 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+        registry.addMapping("/**")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
